99fb70035f1f799a0a7f969abb189a6d259b55a9
[Mitsui311] <[231t257t@stu.kobe-u.ac.jp]>
2024-05-08 18:28:48 +0900

    いろいろテストする用に追加

---
+++ paint_qr/src/Apptest.java
@@ -0,0 +1,31 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class Apptest {
+    public static void main(String[] args) throws Exception {
+
+        Analysisnomal analysis = new Analysisnomal();
+
+        String pathname1 = "C:\\Users\\rikut\\gitcode\\module_paint_QR\\content1.png"; 
+        String pathname2 = "C:\\Users\\rikut\\gitcode\\module_paint_QR\\content2.png"; 
+
+        File file1 = new File(pathname1);
+        BufferedImage image1 = ImageIO.read(file1);
+
+        File file2 = new File(pathname2);
+        BufferedImage image2 = ImageIO.read(file2);
+        BufferedImage compmod = analysis.comp_module_pos1(image1, image2);
+        ImageIO.write(compmod , "png", new File("compmod.png"));
+
+        BufferedImage wtob = analysis.comp_module_pos2(image1, image2);
+        ImageIO.write(wtob , "png", new File("wtob.png"));
+
+
+        int[] changesymmod = analysis.change_symbol_count(image1, image2);
+        System.out.println("changesym" + ":" + analysis.error_symbol_count(image1, image2));
+        System.out.println("whitechangesym" +":" + changesymmod[0]);
+        System.out.println("paintmod" +":" + changesymmod[1]);
+    }
+}
